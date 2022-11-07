33ecdd10d750e7e2f86ba8fd3cca99dfb452a0b3
M-123-456 <mikigerlach2@gmail.com>
2022-11-07 15:13:20 +0100

    signup validation works=

--- backend/src/middlewares/validation.js
+++ backend/src/middlewares/validation.js
@@ -1,12 +1,50 @@
 import { body, validationResult } from 'express-validator'
-import httpErrors from 'http-errors'
+import User from '../models/User.js'
 
-
-const validate = (req, res, next) => {
+export const validate = (req, res, next) => {
     const errors = validationResult(req)
+    console.log(errors)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
     next()
+}
+
+export const userSignupValidationRules = () => {
+    return [
+        body('username')
+            .isLength({ min: 4 })
+            .withMessage('User name should have more than 4 letters'),
+        body('password')
+            .isLength({ min: 6 })
+            .withMessage('Password should have more than 8 letters'),
+        body('username').custom(name => {
+            return User.findOne({ username: name })
+            .then(user => {
+                if(user) {
+                    return Promise.reject('This user name exists already')
+                }
+            })
+        })
+    ]
+}
+
+export const userLoginValidationRules = () => {
+    return [
+        body('username')
+            .isLength({ min: 4 })
+            .withMessage('User name should have more than 4 letters'),
+        body('password')
+            .isLength({ min: 6 })
+            .withMessage('Password should have more than 8 letters'),
+        body('username').custom(name => {
+            return User.findOne({ username: name })
+                .then(user => {
+                    if (!user) {
+                        return Promise.reject('This user does not exist. Please check your input or sign up')
+                    }
+                })
+        })
+    ]
 }
 
--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import 'express-async-errors'
+import { userSignupValidationRules, userLoginValidationRules, validate } from '../middlewares/validation.js'
 
 import * as controller from '../controllers/authController.js'
 
 const router = express.Router()
 
 // Sign up
-router.post('/signup', controller.signup)
+router.post('/signup', userSignupValidationRules(), validate, controller.signup)
 
 // Login
-router.post('/login', controller.login)
+router.post('/login', userLoginValidationRules(), validate, controller.login)
 
 
 export default router
